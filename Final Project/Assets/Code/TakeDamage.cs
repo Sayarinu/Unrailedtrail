0cdd7606bb52fea3f523680223c737f7cbc0a6b8
Nicholas Dell'Aquilo <nickdellaquilo@protonmail.com>
2022-11-10 17:25:17 -0500

    edit rock object

--- Final Project/Assets/Code/TakeDamage.cs
+++ Final Project/Assets/Code/TakeDamage.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class TakeDamage : MonoBehaviour
 {
     [SerializeField] public GameObject wagon;
-
+    [SerializeField] public int rockHealth = 1;
+    public AudioSource rockSmash;
 
     void OnTriggerEnter(Collider other) 
     {
-        wagon.health--;
-        //Screenshake?
+        //wagon.health--;
+        //Screenshake & other effects
+        rockHealth--;
+        if (rockHealth <= 0)
+        {
+            rockSmash.Play(0);
+            Destroy(this);
+        }
+        
+    }
+
+    private void OnDestroy() 
+    {
+        
     }
 }
