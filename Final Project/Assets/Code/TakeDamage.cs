f4db15e851231f72ef29a6617c1704bee29b287a
Nicholas Dell'Aquilo <nickdellaquilo@protonmail.com>
2022-11-11 12:10:36 -0500

    added basic mockup assets

--- Final Project/Assets/Code/TakeDamage.cs
+++ Final Project/Assets/Code/TakeDamage.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class TakeDamage : MonoBehaviour
 {
-    [SerializeField] public GameObject wagon;
+    //[SerializeField] public GameObject wagon;
     [SerializeField] public int rockHealth = 1;
     public AudioSource rockSmash;
 
     private void Awake()
     {
-        //wagon = GetComponent<GameObject>();
-        rockSmash = GetComponent<AudioSource>();
+        //wagon = GameObject.FindGameObjectWithTag("Wagon");
+        //rockSmash = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other) 
     {
-        //Wagon.health--;
+        //wagon.health--;
         //Screenshake & other effects
         rockHealth--;
         if (rockHealth <= 0)
         {
-            rockSmash.Play(0);
-            Destroy(this);
+            rockSmash.Play();
+            Destroy(gameObject);
         }
         
     }
