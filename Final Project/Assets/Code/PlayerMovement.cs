237a8ede1cd45351512b18adb366d32af40effcb
Nicholas Dell'Aquilo <nickdellaquilo@protonmail.com>
2022-11-17 13:59:01 -0500

    working touchscreen joystick on new prefab

--- Final Project/Assets/Code/PlayerMovement.cs
+++ Final Project/Assets/Code/PlayerMovement.cs
@@ -8,8 +8,9 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public VariableJoystick variableJoystick;
     public Rigidbody rb;
-
+    [SerializeField] private bool useTouchscreen = true;
     private CharacterController controller;
+    Vector3 move;
 
     private void Start() 
     {
@@ -19,8 +20,9 @@ public class PlayerMovement : MonoBehaviour
     //public void FixedUpdate()
     public void Update()
     {
-        //Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (useTouchscreen) { move = new Vector3(variableJoystick.Horizontal, 0 , variableJoystick.Vertical); }
+        else                { move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); }
 
         //rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
         controller.Move(move * Time.deltaTime * speed);
