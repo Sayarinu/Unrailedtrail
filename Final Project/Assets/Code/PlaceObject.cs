3ee1e46d44d56228f1d86daaeb3d83f767c6763f
tse-jeff <jeff.tse@nyu.edu>
2022-11-18 10:59:55 -0500

    attempt to put button on screen

--- Final Project/Assets/Code/PlaceObject.cs
+++ Final Project/Assets/Code/PlaceObject.cs
@@ -7,8 +7,18 @@ public class PlaceObject : MonoBehaviour
 
     public GameObject bridge;
     void Update() {
+
         if (Input.GetKeyDown("space")) {
             Instantiate(bridge, new Vector3(GameObject.FindWithTag("Player").transform.position.x - 6f, 0.1f, 0f), Quaternion.identity);
         }
+
+        foreach (Touch touch in Input.touches) {
+            int id = touch.fingerId;
+            if (EventSystem.current.IsPointerOverGameObject(id))
+            {
+                Instantiate(bridge, new Vector3(GameObject.FindWithTag("Player").transform.position.x - 6f, 0.1f, 0f), Quaternion.identity);
+            }
+        }
+
     }
 }
