357fa3ad9abed76774cf9ae3eb8a5d0053788ed6
Sayarinu <bikabattles@gmail.com>
2022-11-18 16:03:37 -0500

    Getting ready for presentation

--- Final Project/Assets/Code/PlaceObject.cs
+++ Final Project/Assets/Code/PlaceObject.cs
@@ -7,27 +7,19 @@ public class PlaceObject : MonoBehaviour
 {
 
     public GameObject bridge;
-    bool gameRunning;
 
-    void Start() {
-        gameRunning = true;
-        StartCoroutine(buildBridge());
-    }
-    IEnumerator buildBridge() {
+    void Update() {
 
         if (Input.GetKeyDown("space")) {
             Instantiate(bridge, new Vector3(GameObject.FindWithTag("Player").transform.position.x - 6f, 0.1f, 0f), Quaternion.identity);
         }
 
-    while (gameRunning) {
         foreach (Touch touch in Input.touches) {
             int id = touch.fingerId;
                 if (EventSystem.current.IsPointerOverGameObject(id))
                 {
                     Instantiate(bridge, new Vector3(GameObject.FindWithTag("Player").transform.position.x - 6f, 0.1f, 0f), Quaternion.identity);
-                    yield return new WaitForSeconds(10);
                 }
             }
         }
-    }
 }
