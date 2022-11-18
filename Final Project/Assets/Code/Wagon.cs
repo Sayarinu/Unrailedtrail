1447cfcba9e6c914a9c030e6c4bd843f3fd6259d
Savitha Nair <savnair36@gmail.com>
2022-11-18 00:31:16 -0500

    Rock now damages carriage

--- Final Project/Assets/Code/Wagon.cs
+++ Final Project/Assets/Code/Wagon.cs
@@ -6,17 +6,6 @@ public class Wagon: MonoBehaviour
 {
     [SerializeField] public float speed = 0.02f;
 
-    [SerializeField] public float health = 100;
-    [SerializeField] public float maxHealth = 100;
-
-    public WagonHealthBar healthbar;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -27,22 +16,4 @@ public class Wagon: MonoBehaviour
     {
         transform.Translate(-speed, 0, 0);
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        print("triggered hit");
-        if (other.CompareTag("Obstacle"))
-        {
-            print("hit obstacle");
-            Damage(5); // obstacles currently deal 5 hp
-        }
-    }
-
-    public void Damage(int amt)
-    {
-        health -= amt;
-        healthbar.UpdateHealthBar();
-    }
-
-
 }
--- Final Project/Assets/Code/WagonHealthBar.cs
+++ Final Project/Assets/Code/WagonHealthBar.cs
@@ -4,8 +4,8 @@ using UnityEngine.UI;
 public class WagonHealthBar : MonoBehaviour
 {
     public Image healthBarImage;
-    public Wagon wagon;
-
+    public WagonDamageController wagon;
+    
     public void UpdateHealthBar()
     {
         healthBarImage.fillAmount = Mathf.Clamp(wagon.health / wagon.maxHealth, 0, 1f);
