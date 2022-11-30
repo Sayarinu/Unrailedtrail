5fbc4844baa6e87f72e586dcb60697eadb835e2b
Sayarinu <bikabattles@gmail.com>
2022-11-30 17:42:30 -0500

    WIP PAUSE MENU

--- Final Project/Assets/Code/Menus/PauseMenu.cs
+++ Final Project/Assets/Code/Menus/PauseMenu.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
+    [SerializeField] GameObject pauseMenu;
 
+    public void Pause() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    
+    public void Resume() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Home(int sceneID) {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
 }
