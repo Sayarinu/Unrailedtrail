c0eba7e730232cb27119439f0a47425433f044d0
Carter Garcia <bikabattles@gmail.com>
2022-11-30 12:24:14 -0500

    Moved around files and am beginning to work on the menus and UI

---
+++ Final Project/Assets/Code/Menus/PauseMenu.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+
+}
---
+++ Final Project/Assets/Code/PublicVars.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class PublicVars
+{
+    public int wood = 0;
+    public int food = 100;
+    public int water = 100;
+}
