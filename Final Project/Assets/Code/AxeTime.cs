ddebb394de9660807dfb0acca376d76b79d8131d
Nicholas Dell'Aquilo <nickdellaquilo@protonmail.com>
2022-12-07 15:05:57 -0500

    axe work

---
+++ Final Project/Assets/Code/AxeTime.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeTime : MonoBehaviour
+{
+    [SerializeField] int timerActive = 60;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        timerActive--
+        if(timerActive == 0) {o}
+    }
+}
