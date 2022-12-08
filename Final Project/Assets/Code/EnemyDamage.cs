d92a82d5007b101e5d0ac25b7ff9e66123da7e7f
Nicholas Dell'Aquilo <nickdellaquilo@protonmail.com>
2022-12-08 11:15:21 -0500

    Update EnemyDamage.cs

--- Final Project/Assets/Code/EnemyDamage.cs
+++ Final Project/Assets/Code/EnemyDamage.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
-    //public PublicVars publicvars = new PublicVars();
+    public PublicVars publicvars = new PublicVars();
     public AudioSource enemyHitSound;
+    [SerializeField] private int foodAmt = 10;
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Weapon")
         {
             enemyHitSound.Play();
+            // enemy.health --;
+            // if (health <= 0) {
+            publicvars.hunger += foodAmt; // when 
             Destroy(gameObject); // or reduce health
+            // }
         }
     }
 }
