09310e2943d613420d0cb203093700740128421f
Savitha Nair <savnair36@gmail.com>
2022-12-08 23:34:51 -0500

    removed thirst feature :(

--- Final Project/Assets/Code/PublicVars.cs
+++ Final Project/Assets/Code/PublicVars.cs
@@ -7,9 +7,6 @@ public static class PublicVars
     public static float hunger = 100;
     public static float hungerMax = 100;
 
-    public static float thirst = 100;
-    public static float thirstMax = 100;
-
     public static float wood = 5;
     public static float woodMax = 10;
 }
